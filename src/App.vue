ea8a1778052940bcb03fb398f11289fae0742147
Cream26 <zhangtao4901@gmail.com>
2024-01-21 14:23:45 +0800

    File configuration

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h1>我是APP</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = "我爱你祖国";
+</script>
+
+<style scoped>
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: red;
+}
+h1 {
+  color: #fff;
+}
+</style>
